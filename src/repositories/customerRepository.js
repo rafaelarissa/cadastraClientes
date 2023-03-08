cfde57f05a1d13a18232eb37e20d65b4ae208072
Rafaela Larissa <rafaela.larissalj@gmail.com>
2023-03-08 01:40:10 -0300

    feat: adding customer repository

---
+++ src/repositories/customerRepository.js
@@ -0,0 +1,26 @@
+import { prisma } from "../config/db.js";
+
+async function insert(nome, cpf, nascimento) {
+  return prisma.clientes.create({
+    data: { nome, cpf, nascimento },
+  });
+}
+
+async function getById(id) {
+  return prisma.clientes.findUnique({ where: { id } });
+}
+
+async function findMany() {
+  return prisma.clientes.findMany();
+}
+
+async function getByCpf(cpf) {
+  return prisma.clientes.findFirst({ where: { cpf } });
+}
+
+export default {
+  insert,
+  getById,
+  getByCpf,
+  findMany,
+};
