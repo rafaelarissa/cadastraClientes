b451fabd82a28d420dc3b1e4c9c09dfa77cb92f0
Rafaela Larissa <rafaela.larissalj@gmail.com>
2023-03-08 01:25:30 -0300

    build: setting up prisma

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import express, { json } from "express";
-import cors from "cors";
+import "express-async-errors";
+import dotenv from "dotenv";
 
-const server = express();
+//import handleErrorsMiddleware from "./middlewares/handleErrors.js";
+//import router from "./routes/index.js";
+
+dotenv.config();
+const app = express();
+app.use(json());
+//app.use(router);
+//app.use(handleErrorsMiddleware);
+
 const PORT = process.env.PORT || 5000;
-
-server.use(cors());
-server.use(json());
-
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}.`);
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
 });
